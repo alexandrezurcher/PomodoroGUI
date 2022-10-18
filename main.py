a1f0c358bcb274785f1d99c748095f6f655c9d79
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-18 09:23:52 +0100

    setting different time sessions and values

--- main.py
+++ main.py
@@ -6,16 +6,37 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    count_down(1 * 15)
+    global reps
+    global timer_label
+    reps+=1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN
+    long_break_min = LONG_BREAK_MIN
+
+    if reps % 8 == 0:
+        count_down(long_break_min)
+        timer_label = Label(text="Break", font=(FONT_NAME, 40, "bold"), fg=RED, bg=YELLOW)
+        timer_label.grid(column=1, row=0)
+
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label = Label(text="Break", font=(FONT_NAME, 40, "bold"), fg=PINK, bg=YELLOW)
+        timer_label.grid(column=1, row=0)
+    else:
+        count_down(work_sec)
+        timer_label = Label(text="WORK", font=(FONT_NAME, 50, "bold"), fg=GREEN, bg=YELLOW)
+        timer_label.grid(column=1, row=0)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -32,6 +53,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -44,7 +67,7 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 50,"bold"), fg=GREEN, bg=YELLOW)
+timer_label = Label(text="Timer", font=(FONT_NAME, 20,"bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(column=1, row=0)
 
 check_mark_label = Label(text="✔", font=(FONT_NAME, 20, "bold"), fg=GREEN, bg=YELLOW)
