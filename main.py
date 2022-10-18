7219e4ff089200bb8b5fc4f44765d259aa55acff
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-18 13:25:09 +0100

    included functionality on the reset button

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 5
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer", font=(FONT_NAME, 40, "bold"),fg=GREEN, bg=YELLOW)
+    check_marks_label.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -49,12 +60,19 @@ def count_down(count):
     if count_sec < 10:
         count_sec = f"0{count_sec}"
 
-
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+            check_marks_label.config(text=marks)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -67,17 +85,17 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 20,"bold"), fg=GREEN, bg=YELLOW)
+timer_label = Label(text="Timer", font=(FONT_NAME, 40,"bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(column=1, row=0)
 
-check_mark_label = Label(text="✔", font=(FONT_NAME, 20, "bold"), fg=GREEN, bg=YELLOW)
-check_mark_label.grid(column=1, row=3)
+check_marks_label = Label(font=(FONT_NAME, 20, "bold"), fg=GREEN, bg=YELLOW)
+check_marks_label.grid(column=1, row=3)
 
 button_start = Button(text="Start", command=start_timer)
 
 button_start.grid(column=0, row=2)
 
-button_reset = Button(text="Reset")
+button_reset = Button(text="Reset", command=reset_timer)
 button_reset.grid(column=2, row=2)
 
 
